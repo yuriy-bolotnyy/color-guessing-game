323ca42e282376894eec94be477f877357de12cd
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-12 17:55:30 -0800

    Init states

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,53 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from './components/Button'
 
-const randomHexColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+const randomHexColor = () => "#" + Math.floor(Math.random()*16777215).toString(16);
 
-function App() {
-  const [hexColor, setHexColor] = useState()
+const randomColorsArray = (len) => {
+  const randomColors = Array.from({length: len}, () => randomHexColor())
+  // console.log("random array: ", randomColors)
+  return randomColors
+}
 
-  // console.log(hexColor )
+const randomColorsArrayWithOneGuess = (arr, guess) => {
+  const randomIndex = Math.ceil(Math.random() * arr.length)
+  arr.splice(randomIndex, 0, guess)
+  return arr
+}
 
-  const randomHexColor = () => "#" + Math.floor(Math.random()*16777215).toString(16);
+// const rc = randomColorsArray(3)
+// console.log(rc)
+// console.log(randomColorsArrayWithOneGuess(randomColorsArray(3-1), "hello"))
 
-  useEffect(() => {
-    setHexColor(randomHexColor())
-  }, []);
 
+function App(props) {
+  const [hexColor, setHexColor] = useState(randomHexColor())
+
+  const guess = hexColor
+  // Insert hexColor into random array of color codes
+  const buttonsArray = randomColorsArrayWithOneGuess(
+    randomColorsArray(props.numberOfGuesses-1), guess)
+
+  const [buttonColorCodes, setButtonColorCodes] = useState(
+    randomColorsArrayWithOneGuess(
+      randomColorsArray(props.numberOfGuesses-1), 
+      hexColor
+    )
+  )
+
+  // useEffect(() => {
+  //   setHexColor(randomHexColor())
+  //   // console.log("Guess color: ", hexColor)
+
+  //   setButtonColorCodes()
+  //   console.log("Number guesses: ", props.numberOfGuesses)
+  //   console.log("Buttons: ", buttonColorCodes)
+
+  // }, []);
+
+  console.log("Guess color: ", hexColor)
+  console.log("Buttons: ", buttonColorCodes)
+  
   // setHexColor(randomHexColor())
 
   const changeColor = () => {
@@ -25,6 +59,11 @@ function App() {
 
   const checkColorGuess = (colorGuess) => {
     console.log("color guess: ", colorGuess)
+    if (colorGuess === hexColor) {
+      console.log("You Won! :)")
+    } else {
+      console.log("Try more ...")
+    }
   }
 
   return (
@@ -33,7 +72,7 @@ function App() {
       </div>
 
       <div className="card">
-        <Button colorcode = "#C90F57" handleClick = {checkColorGuess}/>
+        <Button colorcode = {hexColor} handleClick = {checkColorGuess}/>
 
         <Button colorcode = "#C90F57"/>
 
